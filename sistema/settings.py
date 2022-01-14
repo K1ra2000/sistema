ac2e9ffeb8766eacdf69e9cfc2caefcbc1b6a018
K1ra200 <empresarioorg11@gmail.com>
2022-01-13 22:22:42 -0500

    master

--- sistema/settings.py
+++ sistema/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r&099qigdty55%ri0wqaqu^=t5vzb(w9r3hly406ab4&s9izcm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
